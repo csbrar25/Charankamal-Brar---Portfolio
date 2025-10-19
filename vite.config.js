5ba8e0a13dd5839ce75f4610de9036482173df58
csbrar25 <csbrar25@gmail.com>
2025-10-18 17:27:53 -0700

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL} from 'node:url'
+//import { fileURLToPath, URL} from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    }
-  }
+  base: 'Charankamal-Brar-Portfolio',
 });
 
